00265f722a2c17d42b45aba5cf8dd4e29c1d4539
Tom <teryun93@gmail.com>
2015-04-28 18:45:09 -0700

    good progress

--- app.js
+++ app.js
@@ -4,12 +4,8 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var flash = require('connect-flash');
-
-
-//var session = require('express-session');
-//var cookieParser = require('cookie-parser');
-//var flash = require('express-flash');
-//var cookieParser = require('cookie-parser');
+var server = require('http').Server(app);
+var request = require('request');
 var path = require('path');
 var ejs = require('ejs');
 var Song = require('./models/Song');
@@ -21,20 +17,10 @@ var Virus_Type = require('./models/virus_type');
 var NodeCache = require( "node-cache" );
 var myCache = new NodeCache();
 
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-io.on('connection', function(result){
-    console.log("socket ");
-    console.log(result);
-
-});
-//server.listen(3000);
-
 var app = express();
 app.use(cookieParser('secret'));
 app.use(session({cookie: { maxAge: 60000 }}));
 app.use(flash());
-
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -45,25 +31,7 @@ app.use(bodyParser.urlencoded({
 
 
 
-
-
-/*
-app.configure(function() {
-	app.use(express.cookieDecoder());
-	app.use(express.session());
-});*/
-//app.use(express.bodyParser({uploadDir:'./uploads'}));
-
 app.get('/', function(req, res) {
-    
-    /*
-    console.log("index rednered T");
-    res.render('index', {
-       title: 'Home 1'  
-    });
-    //res.write("hello");
-    //res.end();
-    */
    
     var array = [56,56,56,56,56,78,78,89];
     obj = {variable: array };
@@ -79,25 +47,123 @@ app.get('/', function(req, res) {
       console.log('undefined');
     }
     
-    
     console.log("index rednered T");
     res.render('login', {
-       title: 'Home 1', message: req.flash('info')  
+       title: 'Home 1', message: req.flash('message'), usernameLogin: req.flash('username-login'), passwordLogin: req.flash('password-login'),
+       usernameSignup: req.flash('username-signup'), passwordSignup: req.flash('password-signup'), passwordSignupRepeat:req.flash('password-signup-repeat'),
+       selector: req.flash('selector')
+    });    
+});
+
+app.get('/globe', function(req, res) {
+   
+    cachedViruses = myCache.get( "viruses" );
+    cachedCurrentUser = myCache.get( "currentUser" );
+    
+    if(cachedCurrentUser && cachedViruses)
+    {
+    console.log("cache succeess");  
+    console.log(cachedCurrentUser); 
+    
+    res.render('index', {
+        user: cachedCurrentUser, viruses: cachedViruses
     });
+    }
+    else {
+        
+         res.redirect('/');  
+        
+    }
+  
+});
+
+app.get('/globe/api', function(req, res) {
+    
+    User.hasMany(Virus, {foreignKey: 'user_id'});
+    Virus.belongsTo(User, {foreignKey: 'user_id'});
+    Virus_Type.hasMany(Virus, {foreignKey: 'virus_type_id'});
+    Virus.belongsTo(Virus_Type, {foreignKey: 'virus_type_id'});
+
+    Virus.findAll({
+        include: [{model: User, required: true}, {model:Virus_Type, required: true}],
+        order:'date DESC'
+
+    }).then(function(viruses)
+    {
+
+        var measles_array = [];
+        var ebola_array = [];
+        var malaria_array = [];
+
+        for(i = 0; i < viruses.length; i++)
+        {
+            console.log(viruses[i].virus_type.virus_type);
+            if(viruses[i].virus_type.virus_type == "Ebola")
+            {
+                if(viruses[i].latitude && viruses[i].longitude)
+                {
+                    ebola_array.push(Math.round(viruses[i].latitude));
+                    ebola_array.push(Math.round(viruses[i].longitude));
+                    ebola_array.push(0.5);
+                    console.log("ebola array" + ebola_array);
+                }
+
+            }
+            else if(viruses[i].virus_type.virus_type == "Malaria")
+            {   
+                if(viruses[i].latitude && viruses[i].longitude)
+                {
+                    malaria_array.push(Math.round(viruses[i].latitude));
+                    malaria_array.push(Math.round(viruses[i].longitude));
+                    malaria_array.push(0.5);
+                    console.log("malaria array" + malaria_array);
+                }
+
+            }
+            else if(viruses[i].virus_type.virus_type == "Measles")
+            {
+                if(viruses[i].latitude && viruses[i].longitude)
+                {
+                    measles_array.push(Math.round(viruses[i].latitude));
+                    measles_array.push(Math.round(viruses[i].longitude));
+                    measles_array.push(0.5);
+                    console.log("measles array" + measles_array);
+                }
+
+            }
+        }
+
+        var virus_array = [["Ebola", ebola_array], ["Malaria", malaria_array], ["Measles", measles_array]];
+        console.log('virus array' + virus_array);
+
+        for(i = 0; i < virus_array.length; i++)
+        {
+            console.log(virus_array[i]);
+
+        }     
+        
+        res.json(virus_array);
+ 
+});
+    
+    
     
     
 });
 
-
 app.post('/globe', function(req, res) {
-    //console.log(req);
-    console.log("yoo");
-     req.flash('info', 'hello!');
-     console.log(('info', 'hello!'));
-     
+
     if(req.body.formType == "login")
     {
  
+     if(!req.body.username || !req.body.password)
+     { 
+        req.flash('message', 'Please fill out everything!');
+        req.flash('username-login', req.body.username);
+        req.flash('password-login', req.body.password);  
+        res.redirect('/');  
+         
+     }
      User.find({
         where: {
             username: req.body.username,
@@ -107,35 +173,39 @@ app.post('/globe', function(req, res) {
         console.log(loggedInUser);
         if(loggedInUser)
         {
+            
             User.hasMany(Virus, {foreignKey: 'user_id'});
             Virus.belongsTo(User, {foreignKey: 'user_id'});
             Virus_Type.hasMany(Virus, {foreignKey: 'virus_type_id'});
             Virus.belongsTo(Virus_Type, {foreignKey: 'virus_type_id'});
             
             Virus.findAll({
-                include: [{model: User, required: true}, {model:Virus_Type, required: true}]
+                include: [{model: User, required: true}, {model:Virus_Type, required: true}],
+                order:'date DESC'
                 
             }).then(function(viruses){
-            console.log("viruses");
-            console.log(viruses);
             
+                console.log("viruses");
+                console.log(viruses);
             
-            console.log(loggedInUser);
+            success = myCache.set( "viruses", viruses, 6000);
+            myCache.set("currentUser", loggedInUser, 6000);
             res.render('index', {
                 user: loggedInUser, viruses: viruses
-            }); 
+            });       
+                         
+
+
                       
         });
         }
         else
         {
-
-            //res.send(JSON.stringify(req.flash('test')));
             
-            res.redirect('/');
-            res.render('login', {
-                message: 'Login Failed'
-            });             
+            req.flash('message', 'Login Failed!');
+            req.flash('username-login', req.body.username);
+            req.flash('password-login', req.body.password);
+            res.redirect('/');           
 
         }
      });
@@ -148,21 +218,24 @@ app.post('/globe', function(req, res) {
     if(!req.body.newUsername || !req.body.newPassword || !req.body.newPasswordRepeat)
     {
         
-        console.log("not filled out");
-        res.redirect('/');
-        res.render('login', {
-                message: 'Login Failed'
-        }); 
+        req.flash('message', 'Please fill out all the form');
+        req.flash('username-signup', req.body.newUsername);
+        req.flash('password-signup', req.body.newPassword);
+        req.flash('password-signup-repeat', req.body.newPasswordRepeat);
+        req.flash('selector', 'signup');
+        res.redirect('/'); 
         
     }
     else if(req.body.newPassword != req.body.newPasswordRepeat)
     {
-        console.log("password not mathed");
+      
+        req.flash('message', 'Passwords don not match!');
+        req.flash('username-signup', req.body.newUsername);
+        req.flash('password-signup', req.body.newPassword);
+        req.flash('password-signup-repeat', req.body.newPasswordRepeat);
+        req.flash('selector', 'signup');
         res.redirect('/');
-        /*
-        res.render('login', {
-                            message: 'failed'  
-                    });*/
+        
     }
     else
     {
@@ -177,9 +250,11 @@ app.post('/globe', function(req, res) {
                 if(existingUser)
                 {   
                     
-                    res.render('login', {
-                            message: 'failed'  
-                    });
+                    req.flash('message', 'User already exists!');
+                    req.flash('username-signup', req.body.newUsername);
+                    req.flash('password-signup', req.body.newPassword);
+                    req.flash('password-signup-repeat', req.body.newPasswordRepeat);                    
+                    res.redirect('/');
                     
                 }
                 else
@@ -195,15 +270,15 @@ app.post('/globe', function(req, res) {
                     Virus.belongsTo(Virus_Type, {foreignKey: 'virus_type_id'});
 
                     Virus.findAll({
-                        include: [{model: User, required: true},{model:Virus_Type, required: true}] 
+                        include: [{model: User, required: true},{model:Virus_Type, required: true}],
+                        order:'date DESC'
                     }).then(function(viruses){
                     console.log(viruses);
+
                     //res.send(viruses);
-                    
-                    res.send({'viruses': viruses});
-                    
-                    
-                
+                    myCache.set( "viruses", viruses, 6000);
+                    myCache.set("currentUser", newUser, 6000);
+                    //res.send({'viruses': viruses});
                     res.render('index', {
                         user: newUser, viruses: viruses
                         }); 
@@ -218,48 +293,13 @@ app.post('/globe', function(req, res) {
     console.log("index rednered T");
     
 });
-/*
-app.post('/login', function(req, res) {
-   
-    console.log(req.query.username); 
-    
-     User.find({
-        where: {
-            username: req.body.username,
-            password: req.body.password
-            }
-     }).then(function(loggedInUser) { 
-        console.log(loggedInUser);
-        if(loggedInUser)
-        {
-        User.hasMany(Virus, {foreignKey: 'user_id'})
-        Virus.belongsTo(User, {foreignKey: 'user_id'})
 
-        Virus.findAll({
-            include: [{model: User, required: true}] 
-        }).then(function(viruses){
-            console.log(viruses);
-            console.log(loggedInUser);
-            res.render('index', {
-                user: loggedInUser, viruses: viruses
-            }); 
-                      
-        });
-        }
-        else
-        {
-            res.render('login', {
-                message: 'Login Failed'
-            }); 
- 
-        }
-     });
-     
-    
-});
-*/
-app.post('/Send', function(req, res){ // Specifies which URL to listen for
+app.post('/send', function(req, res){ // Specifies which URL to listen for
 
+    cachedCurrentUser = myCache.get( "currentUser" );
+    //if(cachedCurrentUser)
+    //{
+        
     console.log("yo");
     console.log(req.body.diseaseType);
     console.log("user id" + req.body.userId);
@@ -277,70 +317,49 @@ app.post('/Send', function(req, res){ // Specifies which URL to listen for
     var newVirus = Virus.build();
     newVirus.virus_type_id = req.body.diseaseTypeId;
     newVirus.detail = req.body.detail;
-    newVirus.latitude = req.body.latitude;
-    newVirus.longitude = req.body.longitude;
+    tempLat = Math.round(req.body.latitude);
+    newVirus.latitude = tempLat.toString();
+    tempLng = Math.round(req.body.longitude);
+    newVirus.longitude = tempLng.toString();
     newVirus.location = req.body.location;    
     newVirus.user_id = req.body.userId;
     newVirus.date = currentDate;
     newVirus.save().then(function() {
         
+        //console.log(cachedCurrentUser); 
+            User.hasMany(Virus, {foreignKey: 'user_id'});
+            Virus.belongsTo(User, {foreignKey: 'user_id'});
+            Virus_Type.hasMany(Virus, {foreignKey: 'virus_type_id'});
+            Virus.belongsTo(Virus_Type, {foreignKey: 'virus_type_id'});
+            
+            Virus.findAll({
+                include: [{model: User, required: true}, {model:Virus_Type, required: true}],
+                order:'date DESC'
+                
+            }).then(function(viruses){
+            console.log("viruses");
+            console.log(viruses);
+            
+            //success = myCache.set( "viruses", viruses, 6000);
+            //myCache.set("currentUser", loggedInUser, 1000);
+            console.log(success);
+            res.render('index', {
+                user: cachedCurrentUser, viruses: viruses
+            }); 
+                      
+        });   
+        
+        
     });
-
-});
-
-
-/*
-app.get('/songs', function(req, res) {
-   
-    console.log(req.query.title);
     
-     Song.findAll({
-        where: {
-            title: {
-                like: '%' + req.query.title + '%',
-                playCount: { gt: 0}
-            }
-        },
-        order: 'playCount DESC'
+    //}
+    //else {
+        
+         //res.redirect('/');  
          
-     }).then(function(results) { 
+    //} 
 
-         res.render('songs', {
-            songs: results
-            
-        });         
-     });
-    
 });
-*/
-
-/*
-app.get('/dvds', function(req, res) {
-   
-    console.log(req.query.title); 
-    
-     DVD.findAll({
-        where: {
-            title: {
-                like: '%' + req.query.title + '%'//,
-                //playCount: { gt: 0}
-            },
-            
-            award: {
-                like: '%' + req.query.award + '%'
-            }
-        },
-        order: 'title DESC'
-         
-     }).then(function(results) { 
-
-         res.render('dvds', {
-            dvds: results
-        });         
-     });
-    
-});
-*/
 
 app.listen(3000, function() {
     
